3b792b6388bcfd76a95a77b631ed81f1e9c5b5ae
ytbaby121-eng <ytbaby121@gmail.com>
2026-02-16 23:26:45 +0800

    26220.7755
    26220.7755

--- Windows11_25H2_amd64/uup_download_macos.sh
+++ Windows11_25H2_amd64/uup_download_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-02-16 14:33:54 GMT
+#Generated on 2026-02-16 15:24:47 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=cceff177-5fe6-4eb1-a8cb-b9b528719b8b&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=2b7535d6-c6b8-49d4-a3a8-4317d9af220d&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
